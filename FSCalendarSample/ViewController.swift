809f3b4b1fa7bf7893ac3f576f49c699f2313e2b
“naksuk <n4ksuk@gmail.com>
2021-12-14 23:39:51 +0900

    FSCalendarで過去五日間を塗りつぶす

--- FSCalendarSample/ViewController.swift
+++ FSCalendarSample/ViewController.swift
@@ -1,19 +1,40 @@
 //
 //  ViewController.swift
-//  FSCalendarSample
+//  FSCalendarApp
 //
 //  Created by naksuk on 2021/12/14.
 //
 
 import UIKit
+import FSCalendar
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var calendar: FSCalendar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        calendar.allowsMultipleSelection = true
+        calendar.appearance.todayColor = UIColor.red
+        calendar.appearance.headerTitleColor = UIColor.orange
+        calendar.appearance.weekdayTextColor = UIColor.orange
+        calendar.appearance.selectionColor = UIColor.orange
+        selectDate()
+
     }
-
-
+    
+    private func selectDate() {
+        let now = Date()
+        let year = Calendar.current.component(.year, from: now)
+        let month = Calendar.current.component(.month, from: now)
+        let day = Calendar.current.component(.day, from: now)
+        
+        for i in 0 ..< 5{
+            let selectDay = Calendar.current.date(from: DateComponents(year: year, month: month, day: day - i))
+                calendar.select(selectDay)
+        }
+    }
+    
 }
 
